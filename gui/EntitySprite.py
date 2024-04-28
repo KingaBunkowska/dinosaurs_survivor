5de5aa95e3f84fe2cbb498c8f26ae0ea90c328fd
Kinga Bunkowska <tcx7@o2.pl>
2024-04-29 01:50:47 +0200

    Clean comments from EntitySprite

--- gui/EntitySprite.py
+++ gui/EntitySprite.py
@@ -8,27 +8,12 @@ class EntitySprite:
         self.size, _ = image.get_size() if image!=None else (25,25), 50
         self.image = image
         self.hitbox_size = hitbox_size
-        # self.hitbox = (Position(-10000, -10000), Position(-10000, -10000))
         
-        # self.hitbox_base_coords = (Position(*hitbox_start), Position(hitbox_start[0] + hitbox_size[0], hitbox_start[1] + hitbox_start[1]))
-        # # self.hitbox = (Position(self.entity.position.x - hitbox_start[0], self.entity.position.y - hitbox_start[1]),
-        # #                Position(self.entity.position.x + hitbox_size[0] + hitbox_start[0], self.entity.position.y + hitbox_size[1] + hitbox_start[1]))
-
-        # self.hitbox = (Position(self.entity.position.x - self.hitbox_size[0] / 2, self.entity.position.y - self.hitbox_size[1] / 2),
-        #                Position(self.entity.position.x + self.hitbox_size[0] / 2, self.entity.position.y + self.hitbox_size[1] / 2))
-
         self.hitbox = (Position(self.entity.position.x - self.size[0] / 2, self.entity.position.y - self.size[1] / 2),
                        Position(self.entity.position.x + self.size[0] / 2, self.entity.position.y + self.size[1] / 2))
 
 
-
-    def __update_checkbox(self):
-        # self.hitbox = (self.entity.position - Position(self.size[0]/2, 0) + self.hitbox_base_coords[0], self.entity.position - Position(0, self.size[1]/2) + self.hitbox_base_coords[1])
-        pass
-
     def draw(self, screen):
-        
-        # self.__update_checkbox()
 
         self.hitbox = (Position(self.entity.position.x - self.size[0] / 2, self.entity.position.y - self.size[1] / 2),
                        Position(self.entity.position.x + self.size[0] / 2, self.entity.position.y + self.size[1] / 2))
